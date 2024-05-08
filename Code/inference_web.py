d75102ccfbea1031200a40aa0ec5a8313486d161
Gwihyeon Go <hjh0537@naver.com>
2024-05-08 17:44:04 +0900

    8

--- Code/inference_web.py
+++ Code/inference_web.py
@@ -122,7 +122,8 @@ if st.button('녹음'):
                             channels=CHANNELS,
                             rate=RATE,
                             input=True,
-                            frames_per_buffer=CHUNK)
+                            frames_per_buffer=CHUNK,
+                            input_device_index=0)
 
         frames = []
 
